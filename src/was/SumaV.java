a6677b2f1bc083d048c3d30c0716c9f2de59a1ec
SantiMahecha <smahechap@unal.edu.co>
2019-05-01 19:24:04 -0500

    Update and rename NewClass.java to SumaV.java

--- src/was/SumaV.java
+++ src/was/SumaV.java
@@ -13,26 +13,30 @@ package was;
  */
 public class NewClass {
     public static void main(String[] args) {
-        java.util.Scanner lec= new java.util.Scanner(System.in);
-      System.out.println("tamano");
+        
+      java.util.Scanner lec= new java.util.Scanner(System.in);
+        
+      System.out.println("TAMAÑO");
       int tam=lec.nextInt();
-     int[] vectorB=new int[tam];
+        
+      int[] vectorB=new int[tam];
       int[] vectorA=new int[tam];
-     for(int i=0; i<tam; i++){
+      
+      for(int i=0; i<tam; i++){
+          
          System.out.println("Dato"+i+"A");
                  vectorA[i]=lec.nextInt();
+          
          System.out.println("Dato"+i+"B");
                  vectorB[i]=lec.nextInt();
-     }
-     int[] resultado= SumaVectores(vectorB, vectorB);
-     for (int i=0; i<tam; i++)
-        {
-            System.out.println(resultado[i]);
-        }
+      }
         
-     }
-    
-     static int[] SumaVectores (int[]A, int[]B){
+      int[] resultado= SumaVectores(vectorA, vectorB);
+      for (int i=0; i<tam; i++){
+            System.out.println(resultado[i]);
+      }
+         
+      static int[] SumaVectores (int[]A, int[]B){
          int r[] = new int[A.length];
          for(int i=0; i<A.length; i++){
              r[i]=A[i]+B[i];
